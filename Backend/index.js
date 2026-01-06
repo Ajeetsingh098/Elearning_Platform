c0407aff1179a0fbc046e348ffe7bd41068937da
Ajeet Singh <ajeet@example.com>
2026-01-06 14:01:00 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-    origin:"http://localhost:5173 || https://elearning-platform-1-uwnb.onrender.com",
+    origin:"https://elearning-platform-1-uwnb.onrender.com",
     credentials:true
 })
 )
